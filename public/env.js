d2ee6a7ad01eb89a9e784dc9dc8adca29110df61
seungyoung1 <goldysy91@gmail.com>
2025-11-14 11:19:53 +0900

    port 수정

--- public/env.js
+++ public/env.js
@@ -1,6 +1,6 @@
 // C:\nproject\fe_pdsw\public\env.js
 window.__RUNTIME_CONFIG__ = {
-    API_URL: "http://10.10.30.228/",
+    API_URL: "http://10.10.30.228:8080/",
     RESTAPI_URL: "http://10.10.40.145:8010/",
     LOGIN_API_URL: "https://jedai-qa-web.nexuscommunity.net:9443/agent/loginCubeC",
     DEPLOY_ENV: "production",
