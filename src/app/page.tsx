376708470a145d6158a3164b2afa4dee077903be
seungyoung1 <goldysy91@gmail.com>
2025-12-18 16:34:12 +0900

    메인 화면 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 // src/app/page.tsx
-import { redirect } from 'next/navigation'
+import { redirect } from 'next/navigation';
+import Cookies from 'js-cookie';
 
 // 이후 메인 페이지에 대쉬 보드가 추가 될수도 있을것 같아 이렇게 구성
 export default function MainPage() {
-  redirect('/main')
+  // redirect('/main')
+  const sessionKey = Cookies.get('session_key');
+
+  if (!sessionKey) {
+    redirect('/login'); // 세션 없으면 로그인으로
+  } else {
+    redirect('/main');  // 세션 있으면 메인으로
+  }
 }
